bfd27cbc4b74da8941cfe4f91a1ffbd6e62504de
sounak saha <sounaksaha100@gmail.com>
2021-10-18 23:59:02 +0530

    final commit

--- app/src/main/java/com/codingwithme/googlemap/MainActivity.kt
+++ app/src/main/java/com/codingwithme/googlemap/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.codingwithme.googlemap
 
+import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,10 +22,31 @@ class MainActivity : AppCompatActivity() {
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-        checkLocationPermission()
+        val btn = findViewById<Button>(R.id.button)
+
+        btn.setOnClickListener {
+            fetchLocation()
+        }
+
+
     }
 
-    private fun checkLocationPermission() {
+    private fun fetchLocation() {
+
+        val task =fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)
+        !=PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),101)
+            return
+        }
+        task.addOnSuccessListener {
+            if(it!=null){
+                Toast.makeText(applicationContext,"${it.latitude} ${it.longitude}",Toast.LENGTH_SHORT).show()
+            }
+        }
 
     }
+
 }
